deae3fb0aa127205b3514825d9da132c2dc40508
Unknown <diegoleismann@gmail.com>
2017-09-09 12:58:58 -0300

    Add files

---
+++ content/user.js
@@ -0,0 +1,23 @@
+{
+    "index": 4,
+    "rows": [
+        {
+            "name": "admin",
+            "password": "admin",
+            "created_at": "2017071011113",
+            "_id": "user_1"
+        },
+        {
+            "name": "admin",
+            "password": "admin",
+            "created_at": "2017071011114",
+            "_id": "user_2"
+        },
+        {
+            "name": "admin",
+            "password": "admin",
+            "created_at": "2017071011118",
+            "_id": "user_3"
+        }
+    ]
+}
