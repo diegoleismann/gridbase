d61ac0ae96c0147390f74cc77292dbc19709f38a
Unknown <diegoleismann@gmail.com>
2017-09-09 13:20:16 -0300

    db config

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 fs = require('fs');
 url = require("url");
 var view = require('./layout.js');
@@ -153,4 +156,6 @@ var server = http.createServer(function(request, response){
     show(response, 'Não há methodos post')
   }
 
-}).listen('4500');
+}).listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
